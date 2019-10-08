dc9537e63c7d8baa83c9a22756429574a6be6150
sanggggg <ksme6776@gmail.com>
2019-10-08 14:58:46 +0900

    Generate skeleton structure - only folder - add routing

--- adit-front/src/App.js
+++ adit-front/src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props) {
+    return (
+        <ConnectedRouter history={props.history}>
+            <div className="App">
+                <h1>Adit</h1>
+                <Switch>
+                    <Route path="/login" exact >
+
+                    </Route>
+                    <Redirect from="/" to="login" />
+                </Switch>
+            </div>
+        </ConnectedRouter>
+    );
 }
 
 export default App;
