b05285f694081622626c972a6a66fec09883f847
SeonghwanChoi <csh3695@snu.ac.kr>
2019-10-10 23:24:04 +0900

    Sign up page added

--- adit-front/src/App.js
+++ adit-front/src/App.js
@@ -5,6 +5,7 @@ import { Route, Redirect, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import TopMenu from './components/TopMenu/TopMenu'
 import SignIn from './containers/SignIn/SignIn'
+import SignUp from './containers/SignUp/SignUp'
 
 function App(props) {
     return (
@@ -12,7 +13,8 @@ function App(props) {
             <div className="App">
                 <TopMenu />
                 <Switch>
-                    <Route path="/signin" exact='false' component={SignIn}/>
+                    <Route path="/signin" exact component={SignIn}/>
+                    <Route path="/signup" exact component={SignUp}/>
                     <Route path="/home" exact render={() =><h1>Hello World! This is the main page!</h1>}/>
                     <Route path="/mypage" exact render={() =><h1>MyPage</h1>}/>
                     <Route component={()=>window.location = '/signin'}/>
--- adit-front/src/containers/SignIn/SignIn.js
+++ adit-front/src/containers/SignIn/SignIn.js
@@ -7,14 +7,17 @@ import avatar from '../../assets/avatar.png'
 
 class SignIn extends Component {
     state = {email: '', password: '', name:'', logged_in: false, storedUsers:{email:'csh3695@naver.com', password:'ihateswpp',name:'Kent', logged_in:true}}
-    SigninHandler = () => {
+    SignInHandler = () => {
         if (this.state.storedUsers.email === this.state.email && this.state.storedUsers.password === this.state.password){
-            this.setState({...this.state, email:this.state.storedUsers.email, password:'pw', name:this.state.storedUsers.name, logged_in:true, })
+            this.setState({...this.state, email:this.state.storedUsers.email, password:'', name:this.state.storedUsers.name, logged_in:true, })
             window.location.assign('/home')
             return
         }
         alert('Email or password is wrong')
         return
+    }
+    SignUpHandler = () => {
+        window.location.assign('/signup')
     }
     render(){
         return(
@@ -23,7 +26,7 @@ class SignIn extends Component {
                     <div className='avatar'>
                         <img src={avatar} className="Avatar"/>
                     </div>
-                    <h2 className="text-center">Member Login</h2>   
+                    <h2 className="text-center">Sign In!</h2>   
                     <div className="form-group">
                         <input className="form-control" id='email-input' type='text' value={this.state.email} required="required" onChange={(event)=>this.setState({email:event.target.value})}/>
                     </div>
@@ -31,14 +34,14 @@ class SignIn extends Component {
                         <input className="form-control" id='pw-input' type='password' value={this.state.password} required="required" onChange={(event)=>this.setState({password:event.target.value})}/>
                     </div>        
                     <div className="form-group">
-                        <button type="submit" className="btn btn-primary btn-lg btn-block" button id='login-button' onClick={()=>this.SigninHandler()}>Sign in</button>
+                        <button type="submit" className="btn btn-primary btn-lg btn-block" button id='login-button' onClick={()=>this.SignInHandler()}>Sign in</button>
                     </div>
                     <div className="clearfix">
                         <label clasName="Remember"><input type="checkbox"/> Remember me</label>
                         <a href="#" className="ForgotPW">Forgot Password?</a>
                     </div>
                 </div>
-                <p className="text-center small">Don't have an account? <a href="#">Sign up here!</a></p>
+                <p className="text-center small">Don't have an account? <a href="#" onClick = {() => this.SignUpHandler()}>Sign up here!</a></p>
             </div>
         )
     }
