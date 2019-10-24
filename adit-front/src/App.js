9a46f7efe08b7921760d8ac4501fc41bfb8f1573
sanggggg <ksme6776@gmail.com>
2019-10-24 19:51:36 +0900

    Set Travis CI & ESLint

--- adit-front/src/App.js
+++ adit-front/src/App.js
@@ -3,10 +3,10 @@ import { ButtonToolbar, Button } from 'react-bootstrap';
 import './App.css';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
-import TopMenu from './components/TopMenu/TopMenu'
-import SignIn from './containers/SignIn/SignIn'
-import ArticleSearch from './containers/Article/ArticleSearch/ArticleSearch'
-import SignUp from './containers/SignUp/SignUp'
+import TopMenu from './components/TopMenu/TopMenu';
+import SignIn from './containers/SignIn/SignIn';
+import ArticleSearch from './containers/Article/ArticleSearch/ArticleSearch';
+import SignUp from './containers/SignUp/SignUp';
 import UserInfo from './containers/UserInfo/UserInfo';
 import Home from './containers/Home/Home';
 import ArticleDetail from './containers/Article/ArticleDetail/ArticleDetail';
@@ -20,16 +20,33 @@ function App(props) {
             <div className="App">
                 <TopMenu />
                 <Switch>
-                    <Route path="/signin" exact component={SignIn}/>
-                    <Route path="/signup" exact component={SignUp}/>
-                    <Route path="/home" exact component={Home}/>
-                    <Route path="/adposts/search=:tag" exact component={ArticleSearch}/>
-                    <Route path="/mypage" exact component={UserInfo}/>
-                    <Route path="/article/create" exact component={ArticleCreate}/>
-                    <Route path="/resetpw" exact component={ResetPW}/>
-                    <Route path="/article/:id" exact component={ArticleDetail}/>
-                    <Route path="/article/:id/edit" exact component={ArticleEdit}/>
-                    <Route component={()=>window.location = '/signin'}/>￼￼
+                    <Route path="/signin" exact component={SignIn} />
+                    <Route path="/signup" exact component={SignUp} />
+                    <Route path="/home" exact component={Home} />
+                    <Route
+                        path="/adposts/search=:tag"
+                        exact
+                        component={ArticleSearch}
+                    />
+                    <Route path="/mypage" exact component={UserInfo} />
+                    <Route
+                        path="/article/create"
+                        exact
+                        component={ArticleCreate}
+                    />
+                    <Route path="/resetpw" exact component={ResetPW} />
+                    <Route
+                        path="/article/:id"
+                        exact
+                        component={ArticleDetail}
+                    />
+                    <Route
+                        path="/article/:id/edit"
+                        exact
+                        component={ArticleEdit}
+                    />
+                    <Route component={() => (window.location = '/signin')} />
+                    ￼￼
                 </Switch>
             </div>
         </ConnectedRouter>
