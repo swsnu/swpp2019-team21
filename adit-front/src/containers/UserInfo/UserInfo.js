8092bec92de40be362f5cc92124bfada61b832c7
SeonghwanChoi <csh3695@snu.ac.kr>
2019-10-30 22:22:33 +0900

    Topmenu Click Bug fixed

--- adit-front/src/containers/UserInfo/UserInfo.js
+++ adit-front/src/containers/UserInfo/UserInfo.js
@@ -1,11 +1,20 @@
-import React, { Component, Profiler } from 'react'
-import { Dropdown, DropdownButton, Navbar, Image, OverlayTrigger, Popover, ListGroup, ListGroupItem } from 'react-bootstrap';
-import profile from './../../assets/iu_profile.png'
+import React, { Component, Profiler } from 'react';
+import {
+    Dropdown,
+    DropdownButton,
+    Navbar,
+    Image,
+    OverlayTrigger,
+    Popover,
+    ListGroup,
+    ListGroupItem
+} from 'react-bootstrap';
+import profile from './../../assets/iu_profile.png';
 import { connect } from 'net';
-import UserDetail from '../../components/UserDetail/UserDetail'
+import UserDetail from '../../components/UserDetail/UserDetail';
 import PreviewList from '../../components/PreviewList/PreviewList';
-import BottomBox from '../../components/BottomBox/BottomBox'
-import './UserInfo.css'
+import BottomBox from '../../components/BottomBox/BottomBox';
+import './UserInfo.css';
 import background from '../../assets/userinfo_background.jpg';
 import thumbnail from '../../assets/thumbnail_example.png';
 
@@ -21,27 +30,49 @@ const mockAdPostList = [...Array(10).keys()].map(index => {
 });
 
 class UserInfo extends Component {
-    state = {email:'csh3695@naver.com', fname:'Seonghwan', lname:'Choi', nickname:'Kent', point:0x7fffffff, profileimg:profile, usertag:['student', 'SNU', 'club', 'band'],}
-    tags = this.state.usertag.map((tg) => {return <t>#{tg} </t>})
-    render(){
-        return(
-            <div className='UserInfo'>
-                <img src={background} id='title-background'/>
-                <div className='TitleBox' id='userinfo-titlebox'>
-                    <ttl className='Title' id='userinfo_title'>Hello, {this.state.nickname}!</ttl>
-                    <p><tgs>{this.tags}</tgs></p>
+    state = {
+        email: 'csh3695@naver.com',
+        fname: 'Seonghwan',
+        lname: 'Choi',
+        nickname: 'Kent',
+        point: 0x7fffffff,
+        profileimg: profile,
+        usertag: ['student', 'SNU', 'club', 'band']
+    };
+    tags = this.state.usertag.map(tg => {
+        return <t>#{tg} </t>;
+    });
+    render() {
+        return (
+            <div className="UserInfo">
+                <img src={background} id="title-background" />
+                <div className="TitleBox" id="userinfo-titlebox">
+                    <ttl className="Title" id="userinfo_title">
+                        Hello, {this.state.nickname}!
+                    </ttl>
+                    <p>
+                        <tgs>{this.tags}</tgs>
+                    </p>
                 </div>
-                <div className='AdList'>
-                    <PreviewList articles={mockAdPostList} list_name={'Your Request'} compact={true}/>
-                    <PreviewList articles={mockAdPostList} list_name={'Participated'} compact={true}/>
+                <div className="AdList">
+                    <PreviewList
+                        articles={mockAdPostList}
+                        list_name={'Your Request'}
+                        compact={true}
+                    />
+                    <PreviewList
+                        articles={mockAdPostList}
+                        list_name={'Participated'}
+                        compact={true}
+                    />
                 </div>
                 <UserDetail {...this.state} />
-                <footer className='footer'>
+                <footer className="footer">
                     <BottomBox />
                 </footer>
             </div>
-        )
+        );
     }
 }
 
-export default UserInfo
+export default UserInfo;
