2259308d8fbec868ab31b1a8454a1d8f55ab11da
sanggggg <ksme6776@gmail.com>
2019-11-01 03:13:11 +0900

    Setup react testing configure

---
+++ adit-front/src/components/BottomBox/BottomBox.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import BottomBox from './BottomBox';
+
+const mockedState = {
+    title: 'Mock Title',
+    content: 'Mock content'
+};
+
+describe('<BottomBox />', () => {
+    it('should render without errors', () => {
+        const component = shallow(<BottomBox />);
+    });
+
+    it('should reflect state', () => {
+        const component = shallow(<BottomBox />);
+        const instance = component.instance();
+        instance.setState(mockedState);
+        expect(instance.state.title).toEqual(mockedState.title);
+        expect(instance.state.content).toEqual(mockedState.content);
+    });
+});
