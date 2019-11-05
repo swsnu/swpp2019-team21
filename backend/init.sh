4d5945f120e34788b42bcbce0e0ec3692815dc58
DHdroid <happydh1@snu.ac.kr>
2019-11-06 03:59:59 +0900

    add initial backend data

--- backend/init.sh
+++ backend/init.sh
@@ -3,8 +3,9 @@ source venv/bin/activate
 pip install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
-cd initial_data_set
-cp -b -r initial_media ../media
+cd ./initial_data_set
+rm -rf ../media
+cp -b -rf initial_media ../media
 cd ..
-python manage.py loaddata intial_data_set/initial_data.json
-python runserver
+python manage.py loaddata ./initial_data_set/initial_data.json
+python manage.py runserver
