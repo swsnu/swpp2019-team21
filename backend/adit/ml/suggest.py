373b5c1169283c021815fd9c3528a2ea857dc132
SeonghwanChoi <csh3695@snu.ac.kr>
2019-11-23 00:06:51 +0900

    ML Done!

--- backend/adit/ml/suggest.py
+++ backend/adit/ml/suggest.py
@@ -3,17 +3,20 @@ import numpy as np, pandas as pd
 import random
 from adit.models import *
 
-def update_tag(model, tag_list, shuffle_size):
+model = gensim.models.Word2Vec.load('./adit/ml/models.bin')
+
+def update_tag(tag_list, shuffle_size):
     sentence = []
     for _ in range(shuffle_size):
-        sentence.append(tag_list)
-        random.shuffle(tag_list)
+        mock_list = tag_list[:]
+        random.shuffle(mock_list)
+        sentence.append(mock_list)
     print(sentence)
     model.build_vocab(sentences=sentence, update=True, min_count=shuffle_size)
-    model.train(sentences=sentence,epochs=300, total_examples=shuffle_size)
-    model.save('models.bin')
+    model.train(sentences=sentence, epochs=100, total_examples=shuffle_size)
+    model.save('./adit/ml/models.bin')
 
-def tag_similarity(model, data_exist, data_input):
+def tag_similarity(data_exist, data_input):
     data = data_exist
     suma = 0
     for word in data_input:
@@ -25,10 +28,8 @@ def tag_similarity(model, data_exist, data_input):
     
     return suma/(len(data)*len(data_input))
 
-def tag_suggest(model, data_exist, data_input, TH = -1):
-    #data_exist = ['전시회', '서울대', '박람회', '동아리', '공연', '휴대폰', '태블릿', '노트북스', '사랑', '어린이', '봉사', '날씨', '취업', '개발', '컴퓨터', '개발자', '코딩', '커피', '카페인']
+def tag_suggest(data_exist, data_input, TH = 0.2):
     data_num = np.array(list(map(lambda x : x.postcount, data_exist)))#np.array([80,20,40,65,95,18,20,32,5,10,45,15,85,34,75,85,15,19,68])
-    #data_input = ['연세대', '서울대', '밴드']
     data = pd.DataFrame({'tag':list(map(lambda x : x.content, data_exist)), 'val':data_num/sum(data_num)})
     data_input = list(map(lambda x : x.content, data_input))
     sim = {}
@@ -49,7 +50,7 @@ def tag_suggest(model, data_exist, data_input, TH = -1):
     return list(map(lambda x: x[0], list(filter(lambda x: x[1]>TH, res))[:10]))
 
 
-def post_suggest(model, adposts, data_input, TH = 2e-2):
+def post_suggest(adposts, data_input, TH = 0.2):
     sim = []
     for post in adposts:
         tag = list(map(lambda x:x.content, post.tags.all()))
--- backend/adit/models.py
+++ backend/adit/models.py
@@ -2,7 +2,7 @@ from builtins import ValueError
 from django.db import models
 from django.contrib.auth.models import (
     BaseUserManager, AbstractBaseUser, PermissionsMixin)
-
+from .ml import suggest
 
 class AditUserManager(BaseUserManager):
     def create_user(self, email, nickname, first_name, last_name, tags, password=None):
@@ -174,9 +174,10 @@ class AdPost(models.Model):
         to=InterestedTags,
         related_name='topost'
     )
-"""    def save(self, *args, **kwargs):
+    def save(self, *args, **kwargs):
         super(AdPost, self).save(*args, **kwargs)
-        print(args, kwargs)"""
+        if self.tags.count() > 0:
+            suggest.update_tag(list(map(lambda x: x.content, self.tags.all())), 5)
 
 
 class AdReception(models.Model):
