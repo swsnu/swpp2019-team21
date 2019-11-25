36b3122b2ee0031d9378a05f7e005e419497251f
DHdroid <happydh1@snu.ac.kr>
2019-11-25 22:00:47 +0900

    modify recommendation algorithm

--- backend/adit/ml/suggest.py
+++ backend/adit/ml/suggest.py
@@ -5,6 +5,7 @@ import random, os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 model = gensim.models.Word2Vec.load(BASE_DIR + '/ml/models.bin')
 
+
 def update_tag(tag_list, shuffle_size):
     sentence = []
     for _ in range(shuffle_size):
@@ -15,26 +16,30 @@ def update_tag(tag_list, shuffle_size):
     model.train(sentences=sentence, epochs=100, total_examples=shuffle_size)
     model.save(BASE_DIR + '/ml/models.bin')
 
+
 def tag_similarity(data_exist, data_input):
     data = data_exist
     suma = 0
     for word in data_input:
         for tar in data:
             try:
-                suma += model.wv.similarity(word,tar)
+                sim = model.wv.similarity(word, tar)
+                suma += sim * sim
             except:
                 pass
     res = ''
     try:
-        res = suma/(len(data)*len(data_input))
+        res = suma / (len(data) * len(data_input))
     except ZeroDivisionError:
         return 0
     return res
 
-def tag_suggest(data_exist, data_input, TH = 0.2):
-    data_num = np.array(list(map(lambda x : x.postcount, data_exist)))#np.array([80,20,40,65,95,18,20,32,5,10,45,15,85,34,75,85,15,19,68])
-    data = pd.DataFrame({'tag':list(map(lambda x : x.content, data_exist)), 'val':data_num/sum(data_num)})
-    data_input = list(map(lambda x : x.content, data_input))
+
+def tag_suggest(data_exist, data_input, TH=0.2):
+    data_num = np.array(list(
+        map(lambda x: x.postcount, data_exist)))  # np.array([80,20,40,65,95,18,20,32,5,10,45,15,85,34,75,85,15,19,68])
+    data = pd.DataFrame({'tag': list(map(lambda x: x.content, data_exist)), 'val': data_num / sum(data_num)})
+    data_input = list(map(lambda x: x.content, data_input))
     sim = {}
     for tag in data_input:
         for idx, tar in data.iterrows():
@@ -43,23 +48,23 @@ def tag_suggest(data_exist, data_input, TH = 0.2):
             else:
                 try:
                     if not tar[0] in sim:
-                        sim[tar[0]] = model.wv.similarity(tag,tar[0]) * tar[1]
+                        sim[tar[0]] = model.wv.similarity(tag, tar[0]) * tar[1]
                     else:
-                        sim[tar[0]] += model.wv.similarity(tag,tar[0]) * tar[1]
+                        sim[tar[0]] += model.wv.similarity(tag, tar[0]) * tar[1]
                 except:
                     pass
-    
-    res = sorted(sim.items(), key = lambda x : -x[1])
-    return list(map(lambda x: x[0], list(filter(lambda x: x[1]>TH, res))[:10]))
+
+    res = sorted(sim.items(), key=lambda x: -x[1])
+    return list(map(lambda x: x[0], list(filter(lambda x: x[1] > TH, res))[:10]))
 
 
-def post_suggest(adposts, data_input, TH = 0.2):
+def post_suggest(adposts, data_input, TH=0.2):
     sim = []
     for post in adposts:
-        tag = list(map(lambda x:x.content, post.tags.all()))
-        tag_input = list(map(lambda x:x.content, data_input.all()))
+        tag = list(map(lambda x: x.content, post.tags.all()))
+        tag_input = list(map(lambda x: x.content, data_input.all()))
         sim.append((post, tag_similarity(tag, tag_input)))
 
-    res = sorted(sim, key = lambda x : -x[1])
-    res = list(map(lambda x: x[0].pk, list(filter(lambda x: x[1]>TH, res))[:100]))
+    res = sorted(sim, key=lambda x: -x[1])
+    res = list(map(lambda x: x[0].pk, list(filter(lambda x: x[1] > TH, res))[:100]))
     return res
