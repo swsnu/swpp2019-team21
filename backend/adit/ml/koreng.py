d19678d8883c5a9a1003c40b072fc20adfc35915
SeonghwanChoi <csh3695@snu.ac.kr>
2019-11-25 12:53:56 +0900

    koreng.py fixed

--- backend/adit/ml/koreng.py
+++ backend/adit/ml/koreng.py
@@ -1,17 +1,29 @@
-import numpy as np, pandas as pd, seaborn as sns, matplotlib.pyplot as plt
-import nltk
-import sklearn 
-import torch
-import pickle, os, re, glob, ast
-from matplotlib import font_manager, rc
-
+import numpy as np, pandas as pd
+import pickle, os, re, glob
 import gensim
+import glob
+import konlpy
+import nltk
+from konlpy.tag import Mecab
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.tag import pos_tag
+import random, logging
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+tokenizer = Mecab()
 
 model = gensim.models.Word2Vec.load('ko.bin')
 
-import glob
-
 data = glob.glob('./data/*')
+
+#############################################################################################################
+#############################################################################################################
 
 raw = {}
 raw['KOR'] = []
@@ -25,17 +37,8 @@ for dat in data:
 
 dat = pd.DataFrame(raw)
 
-import konlpy
-from konlpy.tag import Mecab
-
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('averaged_perceptron_tagger')
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-from nltk.tag import pos_tag
-
-tokenizer = Mecab()
+#############################################################################################################
+#############################################################################################################
 
 dat.KOR = dat.KOR.apply(tokenizer.pos)
 dat.ENG = dat.ENG.apply(word_tokenize)
@@ -49,20 +52,22 @@ for i in range(len(dat)):
 for i in range(len(dat)):
     dat.ENG[i] = list(map(lambda x : x[0], list(filter(lambda x : x[1] in ['NN', 'NNP'], dat.ENG[i]))))
 
+#############################################################################################################
+#############################################################################################################
 
 ipt = []
 
 for i in range(len(dat)):
     ipt.append(dat.KOR[i] + dat.ENG[i])
 
-import random, logging
-
 for i in range(len(ipt)):
     random.shuffle(ipt[i])
-    model.build_vocab
+
+#############################################################################################################
+#############################################################################################################
 
 model.build_vocab(sentences=ipt, update=True, min_count=20)
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 model.train(sentences=ipt, epochs=10, total_examples=len(ipt))
 
 model.save('models_engadd.bin')
+
