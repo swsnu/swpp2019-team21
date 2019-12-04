747436785de2ff478adfc39590cd2e7d45bafda2
digdhg <=>
2019-12-04 21:26:02 +0900

    admin user created

--- backend/adit/admin.py
+++ backend/adit/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import AditUser, InterestedTags, PostImage, AdPost, AdReception
 
 # Register your models here.
+admin.site.register(AditUser)
+admin.site.register(InterestedTags)
+admin.site.register(PostImage)
+admin.site.register(AdPost)
+admin.site.register(AdReception)
--- backend/adit/urls.py
+++ backend/adit/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib import admin
 from adit import views
 
 urlpatterns = [
