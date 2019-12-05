91f25271801a9af5a2d3a555c5390bab4445baf0
digdhg <=>
2019-12-05 14:12:23 +0900

    minor bug fixed

--- backend/adit/views.py
+++ backend/adit/views.py
@@ -285,6 +285,8 @@ class AdPostView(View):
         if adpost.ad_link == "toitself":
             adpost.ad_link = 'http://localhost:3000/article/{}/'.format(str(adpost.id))
 
+        adpost.save()
+
         for tag in post_tags:
             if InterestedTags.objects.filter(content=tag).exists():
                 tag_exist = InterestedTags.objects.filter(content=tag)[0]
