d7002b6c375163fa27c7eda80d090ab6dad77f39
SeonghwanChoi <csh3695@snu.ac.kr>
2019-12-12 03:58:48 +0900

    logwrite pause

--- backend/adit/ml/suggest.py
+++ backend/adit/ml/suggest.py
@@ -29,21 +29,21 @@ def update_tag(tag_list, shuffle_size):
 def tag_similarity(data_exist, data_input):
     data = data_exist
     suma = 0
-    log = open(tagsimlog_path, 'a')
-    log.write('$'*10+str(datetime.now())+'$'*10)
+    #log = open(tagsimlog_path, 'a')
+    #log.write('$'*10+str(datetime.now())+'$'*10)
     for word in data_input:
         for tar in data:
             try:
                 sim = 1 + model.wv.similarity(word, tar)
                 suma += sim ** 2
-                log.write('u:{}\te:{}\tsim:{}\n'.format(word, tar, sim**2))
+                #log.write('u:{}\te:{}\tsim:{}\n'.format(word, tar, sim**2))
             except:
                 pass
     res = ''
     try:
         res = suma / (len(data) * len(data_input))
-        log.write('#'*10+'ttlsim:\t{}\n'.format(res))
-        log.close()
+        #log.write('#'*10+'ttlsim:\t{}\n'.format(res))
+        #log.close()
     except ZeroDivisionError:
         return 0
     return res
@@ -67,6 +67,7 @@ def tag_suggest(data_exist, data_input, threshold=0.2):
                     pass
 
     res = sorted(sim.items(), key=lambda x: -x[1])
+    """
     ###forDebugging
     with open(taglog_path, 'a') as log:
         log.write('$' * 10 + str(datetime.now()) + '$' * 10)
@@ -75,6 +76,7 @@ def tag_suggest(data_exist, data_input, threshold=0.2):
             log.write(str(r))
             log.write('\n')
     ###endforDebugging
+    """
     return list(map(lambda x: x[0], list(filter(lambda x: x[1] > threshold, res))[:20]))
 
 
@@ -86,11 +88,15 @@ def post_suggest(adposts, data_input, threshold=0.2):
         sim.append((post, tag_similarity(tag, tag_input)))
 
     res = sorted(sim, key=lambda x: -x[1])
+    """
+    ###forDebugging
     with open(postlog_path, 'a') as log:
         log.write('$' * 10 + str(datetime.now()) + '$' * 10)
         log.write('\nusertag:{}\n'.format([tag.content for tag in data_input.all()]))
         for r in res:
             log.write('\t'+str((r[0].title, [t.content for t in r[0].tags.all()], r[1])))
             log.write('\n')
+    ###endforDebugging
+    """
     res = list(map(lambda x: x[0].pk, list(filter(lambda x: x[1] > threshold, res))[:100]))
     return res
