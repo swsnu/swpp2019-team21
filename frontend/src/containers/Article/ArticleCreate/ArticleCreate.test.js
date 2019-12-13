4d605ce1b8be0bb9ddec5bcc8fba5365c64746ec
DHdroid <happydh1@snu.ac.kr>
2019-12-14 07:29:02 +0900

    testing articlecreate

--- frontend/src/containers/Article/ArticleCreate/ArticleCreate.test.js
+++ frontend/src/containers/Article/ArticleCreate/ArticleCreate.test.js
@@ -103,7 +103,7 @@ describe('<ArticleCreate/>', () => {
         await component.update();
         expect(spyOnGetUser).toHaveBeenCalledTimes(1);
         const temp = component.find('ArticleCreate');
-        temp.setState({ nowpoint: 100 });
+        temp.setState({ nowpoint: 1000 });
         const wrapper = component.find('.article-create');
         expect(wrapper.length).toBe(1);
     });
@@ -113,9 +113,24 @@ describe('<ArticleCreate/>', () => {
             done();
         }, 1000);
         await component.update();
+        let mocked = jest.fn();
+        const mockReader = {
+            onloadend: mocked,
+            readyState: 2,
+            readAsDataURL: mocked,
+            result: 'TEST_RESULT,TEST_RESULT'
+        };
+        mockReader.readAsDataURL = jest.fn(() => {
+            return mockReader.onloadend();
+        });
+        window.FileReader = jest.fn(() => {
+            return mockReader;
+        });
         const temp = component.find('ArticleCreate');
-        temp.setState({ nowpoint: 100 });
+        temp.setState({ nowpoint: 1000 });
         const wrapper = component.find('#confirm-button');
+        const next = component.find('#next-button');
+        const tagnext = component.find('#next-tag-button');
         wrapper.simulate('click');
         expect(spyOnAlert).toHaveBeenCalledTimes(1);
 
@@ -143,8 +158,11 @@ describe('<ArticleCreate/>', () => {
         wrapper.simulate('click');
         expect(spyOnAlert).toHaveBeenCalledTimes(4);
 
-        const mock = new MockFile();
-        const file = mock.create('mocked_image.jpg', 65535, 'image/jpeg');
+        const file = new File(['file contents'], 'TEST_FILE.png', {
+            type: 'image/png'
+        });
+        //const mock = new MockFile();
+        //const file = mock.create('mocked_image.jpg', 65535, 'image/jpeg');
         const thumbnail_wrapper = component.find('#post-thumbnail-input');
         thumbnail_wrapper.simulate('change', {
             target: { files: [file] }
@@ -172,7 +190,7 @@ describe('<ArticleCreate/>', () => {
 
         wrapper.simulate('click');
         expect(spyOnAlert).toHaveBeenCalledTimes(7);
-
+        next.at(0).simulate('click')
         varConfirm = true;
         wrapper.simulate('click');
         expect(spyOnAlert).toHaveBeenCalledTimes(8);
@@ -184,22 +202,26 @@ describe('<ArticleCreate/>', () => {
         const mocktags = ['SNU', 'Mock tags', 'HaHa'];
         detailinstance.handleAddition(mocktags);
         detailinstance.handleDelete(1);
-
+        tagnext.simulate('click')
         const goal_wrapper = component.find('#post-goal-input');
         goal_wrapper.simulate('change', {
             target: { value: 'a' }
         });
         goal_wrapper.simulate('change', {
-            target: { value: '1' }
+            target: { value: '10' }
         });
 
+        wrapper.simulate('click');
+        goal_wrapper.simulate('change', {
+            target: { value: '100' }
+        });
         wrapper.simulate('click');
         expect(spyOnAlert).toHaveBeenCalledTimes(10);
 
         const calendar_wrapper = component.find('#select-calender');
         calendar_wrapper.simulate('change', {
             getYear: () => {
-                return 119;
+                return 2030;
             },
             getMonth: () => {
                 return 1;
@@ -208,5 +230,7 @@ describe('<ArticleCreate/>', () => {
                 return 1;
             }
         });
+        next.at(1).simulate('click')
+        wrapper.simulate('click');
     });
 });
