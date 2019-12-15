743a76f4b6f0389b266f9a2ef090ca521913b6c1
digdhg <=>
2019-12-16 00:00:33 +0900

    Logging paused

--- backend/adit/ml/suggest.py
+++ backend/adit/ml/suggest.py
@@ -8,9 +8,11 @@ import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 model = gensim.models.Word2Vec.load(BASE_DIR + '/ml/model_v1.0.bin')
+"""
 taglog_path=BASE_DIR + '/log/tag_suggest.log'
 postlog_path=BASE_DIR + '/log/post_suggest.log'
 tagsimlog_path=BASE_DIR + '/log/tag_similarity.log'
+"""
 
 def update_tag(tag_list, shuffle_size):
     sentence = []
@@ -29,21 +31,23 @@ def update_tag(tag_list, shuffle_size):
 def tag_similarity(data_exist, data_input):
     data = data_exist
     suma = 0
+    """
     log = open(tagsimlog_path, 'a')
     log.write('$'*10+str(datetime.now())+'$'*10)
+    """
     for word in data_input:
         for tar in data:
             try:
                 sim = 1 + model.wv.similarity(word, tar)
                 suma += sim ** 2
-                log.write('u:{}\te:{}\tsim:{}\n'.format(word, tar, sim**2))
+                #log.write('u:{}\te:{}\tsim:{}\n'.format(word, tar, sim**2))
             except:
                 pass
     res = ''
     try:
         res = suma / (len(data) * len(data_input))
-        log.write('#'*10+'ttlsim:\t{}\n'.format(res))
-        log.close()
+        #log.write('#'*10+'ttlsim:\t{}\n'.format(res))
+        #log.close()
     except ZeroDivisionError:
         return 0
     return res
@@ -66,7 +70,7 @@ def tag_suggest(data_exist, data_input, threshold=0.1):
                 pass
 
     res = sorted(sim.items(), key=lambda x: -x[1])
-    
+    """    
     ###forDebugging
     with open(taglog_path, 'a') as log:
         log.write('$' * 10 + str(datetime.now()) + '$' * 10)
@@ -75,7 +79,7 @@ def tag_suggest(data_exist, data_input, threshold=0.1):
             log.write(str(r))
             log.write('\n')
     ###endforDebugging
-    
+    """    
     return list(map(lambda x: x[0], list(filter(lambda x: x[1] > threshold, res))[:5]))
 
 
@@ -87,7 +91,7 @@ def post_suggest(adposts, data_input, threshold=0.2):
         sim.append((post, tag_similarity(tag, tag_input)))
 
     res = sorted(sim, key=lambda x: -x[1])
-    
+    """
     ###forDebugging
     with open(postlog_path, 'a') as log:
         log.write('$' * 10 + str(datetime.now()) + '$' * 10)
@@ -96,6 +100,6 @@ def post_suggest(adposts, data_input, threshold=0.2):
             log.write('\t'+str((r[0].title, [t.content for t in r[0].tags.all()], r[1])))
             log.write('\n')
     ###endforDebugging
-    
+    """
     res = list(map(lambda x: x[0].pk, list(filter(lambda x: x[1] > threshold, res))[:100]))
     return res
